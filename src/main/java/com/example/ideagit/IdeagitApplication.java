2d73c556c1ebdb58cf0ff33c91b2c82938102027
hyx <test@hyx.com>
2020-08-06 18:19:50 +0800

    第一次提交

---
+++ src/main/java/com/example/ideagit/IdeagitApplication.java
@@ -0,0 +1,13 @@
+package com.example.ideagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeagitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeagitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ideagit/IdeagitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ideagit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeagitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
